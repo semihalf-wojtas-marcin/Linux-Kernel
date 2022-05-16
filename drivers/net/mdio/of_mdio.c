4383d56b04366af74fcd553d34b69cc0e2e49ed8
Marcin Wojtas <mw@semihalf.com>
2022-05-16 17:21:02 +0200

    net: phy: fixed_phy: switch to fwnode_ API
    This patch allows to use fixed_phy driver and its helper
    functions without Device Tree dependency, by swtiching from
    of_ to fwnode_ API.
    
    Signed-off-by: Marcin Wojtas <mw@semihalf.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>

--- drivers/net/mdio/of_mdio.c
+++ drivers/net/mdio/of_mdio.c
@@ -421,7 +421,7 @@ int of_phy_register_fixed_link(struct device_node *np)
 	return -ENODEV;
 
 register_phy:
-	return PTR_ERR_OR_ZERO(fixed_phy_register(PHY_POLL, &status, np));
+	return PTR_ERR_OR_ZERO(fixed_phy_register(PHY_POLL, &status, of_fwnode_handle(np)));
 }
 EXPORT_SYMBOL(of_phy_register_fixed_link);
 
