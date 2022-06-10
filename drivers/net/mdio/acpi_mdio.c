683fb7d4d7571a9b7b81cd3ceb4ed7e2908ec695
Marcin Wojtas <mw@semihalf.com>
2022-06-10 14:24:30 +0200

    net: mdio: allow registering non-PHY devices in ACPI world
    This patch utilizes the newly added fwnode_mdiobus_register_device
    function and enables registration of non-PHY MDIO devices.
    For that purpose a helper routine is added, allowing to determine,
    whether the device associated to ACPI node is a PHY.
    In addition to that update, allow matching child devices' drivers
    based on their ACPI ID.
    
    Signed-off-by: Marcin Wojtas <mw@semihalf.com>

--- drivers/net/mdio/acpi_mdio.c
+++ drivers/net/mdio/acpi_mdio.c
@@ -18,6 +18,41 @@ MODULE_AUTHOR("Calvin Johnson <calvin.johnson@oss.nxp.com>");
 MODULE_LICENSE("GPL");
 
 /**
+ * acpi_mdiobus_child_is_phy - check if device associated with fwnode is a PHY.
+ * @fwnode: pointer to MDIO bus child fwnode and is expected to represent ACPI
+ * device object.
+ *
+ * The function returns true if the child node is for a PHY.
+ * It must comprise either:
+ * o Compatible string of "ethernet-phy-idX.X"
+ * o Compatible string of "ethernet-phy-ieee802.3-c45"
+ * o Compatible string of "ethernet-phy-ieee802.3-c22"
+ * o No _HID or _CID fields.
+ */
+static bool acpi_mdiobus_child_is_phy(struct fwnode_handle *child)
+{
+	struct acpi_device *adev = to_acpi_device_node(child);
+	u32 phy_id;
+
+	if (fwnode_get_phy_id(child, &phy_id) != -EINVAL)
+		return true;
+
+	if (fwnode_property_match_string(child, "compatible",
+					 "ethernet-phy-ieee802.3-c45") == 0)
+		return true;
+
+	if (fwnode_property_match_string(child, "compatible",
+					 "ethernet-phy-ieee802.3-c22") == 0)
+		return true;
+
+	/* Default to PHY if no _HID or _CID found in the fwnode. */
+	if (list_empty(&adev->pnp.ids))
+		return true;
+
+	return false;
+}
+
+/**
  * acpi_mdiobus_register - Register mii_bus and create PHYs from the ACPI ASL.
  * @mdio: pointer to mii_bus structure
  * @fwnode: pointer to fwnode of MDIO bus. This fwnode is expected to represent
@@ -47,7 +82,10 @@ int acpi_mdiobus_register(struct mii_bus *mdio, struct fwnode_handle *fwnode)
 		if (ret || addr >= PHY_MAX_ADDR)
 			continue;
 
-		ret = fwnode_mdiobus_register_phy(mdio, child, addr);
+		if (acpi_mdiobus_child_is_phy(child))
+			ret = fwnode_mdiobus_register_phy(mdio, child, addr);
+		else
+			ret = fwnode_mdiobus_register_device(mdio, child, addr);
 		if (ret == -ENODEV)
 			dev_err(&mdio->dev,
 				"MDIO device at address %d is missing.\n",
